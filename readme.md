48779dbf55d391689e9562ab7953c04e60caef39
Marijnone <marijn.moviat@hva.nl>
2018-11-19 11:50:22 +0100

    code change
    This code works and gets the publication date, and plots in correct

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 ### Concepts & Sketches
 
 A timeline chart 
+
+Where people can click on an author and see their live timeline. On the x it plots their books and their lifecycle. On the y it plots the authors
+
+![Wireframe](./img/sketch.jpg "Sketch")
+
+
+![Wireframe](./img/wireframe.png "Wireframe")
--- src/viz.js
+++ src/viz.js
@@ -1,6 +1,9 @@
-d3.json('./json/data.json').then(function (data) {
+d3.json('./json/json.json').then(function (data) {
 
     var speceficAuthors = ["Charles Dickons", "Mark Twain", "Stephen King"]
+
+
+    const lessBooks = d3.shuffle(data).slice(0, 15)
 
     const filteredByAuthors = d3.nest().key(book => {
         return book.author.fullname
@@ -9,15 +12,70 @@ d3.json('./json/data.json').then(function (data) {
     const filtered = filteredByAuthors.filter(author => {
         if (speceficAuthors.indexOf(author.key) > -1) {
             console.log('GEVONDEN')
-            return author
+            return {
+                author
+            }
         }
         return
     })
 
-    console.log(filtered);
+    const maxYear = d3.max(lessBooks, d => d.publication)
+    const minYear = d3.min(lessBooks, d => d.publication)
+
+    console.log(filtered)
+
+    var margin = {
+        top: 80,
+        right: 50,
+        bottom: 80,
+        left: 50
+    }
+
+    const width = window.innerWidth - margin.left - margin.right
+    const height = window.innerHeight - margin.top - margin.bottom
+
+    d3.select("body")
+        .append("svg")
+    const svg = d3.select('svg')
+
+    svg
+        .attr('width', width)
+        .attr('height', height)
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+
+    var scale = d3.scaleLinear() // v4
+        .domain([minYear, maxYear])
+        .range([0, width])
+    // clipped
+    var axisGroup = svg.append("g");
+    var axis = d3.axisBottom() // v4
+        .scale(scale)
+        .tickFormat(d3.format('y'))
+        .ticks(maxYear - minYear)
+    axisGroup.call(axis);
+
+    svg.selectAll("rects")
+        .data(lessBooks)
+        .enter()
+        .append("rect")
+        .attr("width", 15)
+        .attr("height", 15)
+        .attr("y", -15)
+        .attr("x", book => {
+            return scale(book.publication)
+            console.log(book.publication);
+
+        })
+        .style("fill", "red")
+
+
+
+
+
 
     let filteredBooks = filtered.map(filtered => {
-        // console.log(filtered);
+        console.log(filteredBooks);
 
         return {
             year: books.publication.year,
@@ -29,54 +87,31 @@ d3.json('./json/data.json').then(function (data) {
 
     })
 
-    return dataStore
+    return
 
 
     //Make the SVG
-    const width = 900
-
-    const svg = d3.select("#chart-area")
-        .append("svg")
-        .attr("width", "900")
-        .attr("height", "400");
-
-    const maxValue = d3.max(data)
-    const minValue = d3.min(data)
-    // console.log(data);
 
 
-    // console.log(d.publication);
 
 
-    const scale = d3.scaleLinear()
-        .domain([0, maxValue])
-        .range([0, width])
+    // const maxValue = d3.max(data)
+    // const minValue = d3.min(data)
+    // // console.log(data);
 
-    const axisGroup = svg.append("g")
-    const axis = d3.axisBottom()
-        .scale(scale)
-    axisGroup.call(axis)
+
+    // // console.log(d.publication);
+
+
+    // const scale = d3.scaleLinear()
+    //     .domain([0, maxValue])
+    //     .range([0, width])
+
+    // const axisGroup = svg.append("g")
+    // const axis = d3.axisBottom()
+    //     .scale(scale)
+    // axisGroup.call(axis)
 
 
 
 })
-
-// const rects = svg.selectAll("rect")
-//     .data(data)
-//     .enter().append("rect")
-//     .attr("y", 0)
-//     .attr("x", function (d, i) {
-//         return (i * 40);
-//     })
-//     //return the width of the books with d.description = nummber of pages
-//     .attr("width", function (d, i) {
-//         return x(d.description)
-//     })
-//     .attr("height", function (d) {
-//         return 60;
-//     })
-//     .attr("fill", function (d) {
-//         // return "grey";
-//     })
-//     .on('mouseover', tip.show)
-//     .on('mouseout', tip.hide)
